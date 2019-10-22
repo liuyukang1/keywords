cc37a32183632c39a31b979a084fcdf4c9e353c9
e <785342405@qq.com>
2019-10-23 00:51:10 +0800

    Initial commit

---
+++ src/main/java/com/example/keywords/KeywordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.keywords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeywordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keywords/KeywordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keywords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
